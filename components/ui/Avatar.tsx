398f8520b97e5d9d769a4b2340d68d88a4ce1a93
frecapG7 <florian.recape@wanadoo.fr>
2026-02-11 18:00:59 +0100

    Events p.3: ui improvements (#26)

--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -9,7 +9,7 @@ type ImageSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 const sizeToClassMap = {
     xs: 'w-6 h-6',
-    sm: 'w-8 h-8',
+    sm: 'w-10 h-10',
     md: 'w-16 h-16',
     lg: 'w-24 h-24',
     xl: 'w-36 h-36'
